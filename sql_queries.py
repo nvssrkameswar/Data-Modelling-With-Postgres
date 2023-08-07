ad3395e1a01dc68bc606435d8e0ec8ac5c503506
nvssrkameswar <nvssrkameswar@gmail.com>
2023-08-07 14:07:11 +0530

    first commit

---
+++ sql_queries.py
@@ -0,0 +1,98 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE  IF EXISTS fact_songplays"
+user_table_drop = "DROP TABLE IF EXISTS  dim_users"
+song_table_drop = "DROP TABLE IF EXISTS  dim_songs"
+artist_table_drop = "DROP TABLE  IF EXISTS dim_artists"
+time_table_drop = "DROP TABLE  IF EXISTS dim_time"
+
+# CREATE TABLES
+
+songplay_table_create = ("""CREATE TABLE IF NOT EXISTS fact_songplays(
+	songplay_id SERIAL CONSTRAINT songplay_pk PRIMARY KEY,
+	start_time TIMESTAMP REFERENCES dim_time (start_time),
+	user_id INT REFERENCES dim_users (user_id),
+	level VARCHAR NOT NULL,
+	song_id VARCHAR REFERENCES dim_songs (song_id),
+	artist_id VARCHAR REFERENCES dim_artists (artist_id),
+	session_id INT NOT NULL, 
+	location VARCHAR,
+	user_agent TEXT
+)""")
+
+user_table_create = ("""CREATE TABLE IF NOT EXISTS  dim_users(
+	user_id  INT CONSTRAINT users_pk PRIMARY KEY,
+	first_name  VARCHAR,
+	last_name  VARCHAR,
+	gender  CHAR(1),
+	level VARCHAR NOT NULL
+)""")
+
+song_table_create = ("""CREATE TABLE  IF NOT EXISTS dim_songs(
+	song_id VARCHAR CONSTRAINT songs_pk PRIMARY KEY,
+	title  VARCHAR,
+	artist_id  VARCHAR REFERENCES dim_artists (artist_id),
+	year INT CHECK (year >= 0),
+	duration FLOAT
+)""")
+
+artist_table_create = ("""CREATE TABLE  IF NOT EXISTS dim_artists(
+	artist_id VARCHAR CONSTRAINT artist_pk PRIMARY KEY,
+	name VARCHAR,
+	location VARCHAR,
+	latitude DECIMAL(9,6),
+	longitude DECIMAL(9,6)
+)""")
+
+time_table_create = ("""CREATE TABLE IF NOT EXISTS  dim_time(
+	start_time  TIMESTAMP CONSTRAINT time_pk PRIMARY KEY,
+	hour INT NOT NULL CHECK (hour >= 0),
+	day INT NOT NULL CHECK (day >= 0),
+	week INT NOT NULL CHECK (week >= 0),
+	month INT NOT NULL CHECK (month >= 0),
+	year INT NOT NULL CHECK (year >= 0),
+	weekday VARCHAR NOT NULL
+)""")
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""INSERT INTO fact_songplays VALUES (DEFAULT, %s, %s, %s, %s, %s, %s, %s, %s )
+""")
+
+
+# Updating the user level on conflict
+user_table_insert = ("""INSERT INTO dim_users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s) 
+                        ON CONFLICT (user_id) DO UPDATE SET 
+                        level = EXCLUDED.level 
+""")
+
+song_table_insert = ("""INSERT INTO dim_songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s) 
+                        ON CONFLICT (song_id) DO NOTHING                        
+""")
+
+
+# Artist location, latitude and longitude might change and need to be updated.
+artist_table_insert = ("""INSERT INTO dim_artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s) 
+                          ON CONFLICT (artist_id) DO UPDATE SET
+                          location = EXCLUDED.location,
+                          latitude = EXCLUDED.latitude,
+                          longitude = EXCLUDED.longitude
+""")
+
+time_table_insert = ("""INSERT INTO dim_time VALUES (%s, %s, %s, %s, %s, %s, %s) ON CONFLICT (start_time) DO NOTHING
+""")
+
+# FIND SONGS
+
+song_select = ("""
+    SELECT song_id, dim_artists.artist_id
+    FROM dim_songs JOIN dim_artists ON dim_songs.artist_id = dim_artists.artist_id
+    WHERE dim_songs.title = %s
+    AND dim_artists.name = %s
+    AND dim_songs.duration = %s
+""")
+
+# QUERY LISTS
+
+create_table_queries = [user_table_create, artist_table_create, song_table_create, time_table_create, songplay_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
